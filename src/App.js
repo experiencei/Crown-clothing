245ee0cae819120421ce0c7570b7cda0cc06f298
experiencei <ayelojahighbee01@gmail.com>
2021-09-05 14:01:48 +0100

    adding the haeder component and the Logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
  import './App.css';
  import Homepages from './components/directory/directory';
  import Shoppage from './pages/shoppages/shoppage';
+ import Header from './components/haeder/header';
  import { Route ,Switch } from "react-router-dom";
 
  
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path="/" component={ Homepages }/>
       <Route  exact path="/shop" component={ Shoppage }/>
