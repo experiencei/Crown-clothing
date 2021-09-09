bf5e3663eef9eb79d46e2fc16dd29180716bc3d0
experiencei <ayelojahighbee01@gmail.com>
2021-09-09 19:24:58 +0100

    created the dropdown with the items from cart reducer

--- src/components/cart-dropdown/cart-dropdowm.jsx
+++ src/components/cart-dropdown/cart-dropdowm.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import Custombtn from "../customButton/custombtn";
+import Cartitem from "../cart-item/cart-item"
+import { connect } from 'react-redux'
 import "./cart-dropdown.scss";
 
-export default function Cartdropdowm() {
+ function Cartdropdowm({cart }) {
     return (
     <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {
+        cart.map(carted => 
+        <Cartitem key={carted.id} item={carted}/>
+        )
+      }
+    </div>
     <Custombtn>GO TO CHECKOUT</Custombtn>
   </div>
     )
 }
+
+const mapStateToProps = ({cart : {cart}}) => ({
+  cart
+})
+
+
+
+
+export default connect(mapStateToProps)(Cartdropdowm);
